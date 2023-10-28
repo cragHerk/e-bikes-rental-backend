e7e86b6e99f6882b478e1bf7dd09cbe1bb8d00a7
m.backi <108221259+cragHerk@users.noreply.github.com>
2023-10-28 21:31:42 +0200

    Update emailTemplatesClient.js

--- email/emailTemplatesClient.js
+++ email/emailTemplatesClient.js
@@ -16,9 +16,10 @@ const generateBookingClientEmail = (reservation) => {
     totalPrice,
   } = reservation;
 
-  const subject = "Potwierdzenie rezerwacji";
+  const subject = "Potwierdzenie rezerwacji KamRock";
   const html = `
-    <h2>Potwierdzenie rezerwacji</h2>
+    <h2>Hej tu Kamil z wypożyczalni rowerów</h2>
+    <h3>Oto potwierdzenie Twojej rezerwacji</h3>
     <p><strong>Imię:</strong> ${firstName}</p>
     <p><strong>Nazwisko:</strong> ${lastName}</p>
     ${companyName ? `<p><strong>Firma:</strong> ${companyName}</p>` : ""}
@@ -33,7 +34,9 @@ const generateBookingClientEmail = (reservation) => {
     <p><strong>Data końcowa:</strong> ${endDate}</p>
     <p><strong>Cena:</strong> ${totalPrice} zł</p>
     <h4>Wypożyczalnia rowerów elektrycznych Słotwina</h4>
-    <p>kontakt : 123-321-123</p>
+    <p>Masz pytanie, zadzwoń !  : 123-321-123</p>
+    <p>Do zobaczenia: KamRock - wypożyczalnia rowerów elektrycznych </p>
+    <p> adres : Słotwina 123</p>
 `;
 
   return { subject: subject, html: html, email: email };
