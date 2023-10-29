cf96dbe4218728f7e0bc8d18f4ca17f4dc024c25
m.backi <108221259+cragHerk@users.noreply.github.com>
2023-10-29 11:11:29 +0100

    Update emailTemplatesClient.js

--- email/emailTemplatesClient.js
+++ email/emailTemplatesClient.js
@@ -22,13 +22,6 @@ const generateBookingClientEmail = (reservation) => {
     <h3>Oto potwierdzenie Twojej rezerwacji:</h3>
     <p><strong>Imię:</strong> ${firstName}</p>
     <p><strong>Nazwisko:</strong> ${lastName}</p>
-    ${companyName ? `<p><strong>Firma:</strong> ${companyName}</p>` : ""}
-    <p><strong>Kraj:</strong> ${country}</p>
-    <p><strong>Ulica:</strong> ${street}</p>
-    <p><strong>Kod pocztowy:</strong> ${postalCode}</p>
-    <p><strong>Miasto:</strong> ${city}</p>
-    <p><strong>Telefon:</strong> ${phone}</p>
-    <p><strong>Email:</strong> ${email}</p>
     <p><strong>Model roweru:</strong> ${bikeModel}</p>
     <p><strong>Data początkowa:</strong> ${startDate}</p>
     <p><strong>Data końcowa:</strong> ${endDate}</p>
